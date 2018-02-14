4307ee50e45d02a254e3c2a66930e4b3dc98d5a8
Raul Ramirez Gomez <raulramirezgomez@gmail.com>
2018-02-14 12:00:27 +0100

    Observer options added

--- COAPclient.py
+++ COAPclient.py
@@ -4,39 +4,12 @@ import random
 import re
 import time
 
-TRUE = 1
-FALSE = 0
-
-def random_server():
-	server=["aaaa::212:4b00:7e1:c280", "aaaa::212:4b00:7e1:d086"]
-	x = random.randint(1,2)
-	return(server[x%2])
-# End of random_server()
-
-
-def random_payload():
-	path=["/sen/temp","/sen/volt"]
-	x = random.randint(1,2)
-	return(path[x%2])
-# End of random_payload()
-
-
-def create_pkt():
-	host = random_server()
-	#host = "aaaa::212:4b00:7e1:d086"
-	udp_port = 5683
-	#payload = "/sen/temp"
-	payload = random_payload()
-
-	return host, udp_port, payload
-# End of create_pkt()
-
+serverRequest = None
 
 def write_file(response):
-	flag=TRUE
+	flag=True
 	parserFullLine = re.compile("From \('(.*)', (\d+)\).* ({.*):?\.\.\.")
 	fullLine=parserFullLine.findall(str(response))
-
 	if len(fullLine):
 		if len(fullLine[0])==3:
 			parserType = re.compile("{\"(.*)\":{")
@@ -46,16 +19,16 @@ def write_file(response):
 			elif (len(fieldType) and fieldType[0]=="Temp"):
 				unit="\"C\""
 			else:
-				flag=FALSE
+				flag=False
 				print "[**ERROR**] There is an error while getting Type value.\n"
 
-			if flag==FALSE:
+			if flag==False:
 				newLine = "[**ERROR**] Error parsing this line!"
 			else:
 				measure = (fullLine[0][2], "}}")
 				newLine = "From: ({0}:{1}), Receive: {2}, Date: {3}\n".format(fullLine[0][0], fullLine[0][1], unit.join(measure), time.ctime())
 			
-			with open('file.txt', 'a') as f:
+			with open('results.txt', 'a') as f:
 				f.write(newLine)
 		else:
 			print "[**ERROR**] There is an error while parsing full line.\n"
@@ -63,19 +36,87 @@ def write_file(response):
 		print "[**ERROR**] The parsed full line is empty!\n"
 # End of write_file()
 
-def main():
-	while(1):
-		host, port, path = create_pkt()
-		print "[{0}] Making GET request: [{1}]:{2}{3}".format(time.ctime(), host,port,path)
-		serverRequest = HelperClient(server=(host, port))
-		if serverRequest:
-			response = serverRequest.get(path)
-			if response:
-				serverRequest.stop()
-				write_file(response)
+
+def create_pkt():
+	host = random.choice(["aaaa::212:4b00:7e1:c280", "aaaa::212:4b00:7e1:d086"])
+	udp_port = 5683
+	payload = random.choice(["/sen/temp","/sen/volt"])
+
+	return host, udp_port, payload
+# End of create_pkt()
+
+
+def observer_func():
+	global serverRequest
+	host, port, path = create_pkt()
+	print "[{0}] Making an observer request: [{1}]:{2}{3}".format(time.ctime(), host,port,path)
+	serverRequest = HelperClient(server=(host, port))
+	if serverRequest:
+		serverRequest.observe(path, client_callback_observe)
+# End of observer_func()
+
+
+def client_callback_observe(response):
+	global serverRequest
+	print response
+	write_file(response)
+	check=True
+	while check:
+		option = raw_input("Stop observing? [y/N]: ")
+		if option != "" and not (option.lower() == "y" or option.lower() == "n"):
+			print "Unrecognized choose."
+			continue
+		elif option.lower() == "y":
+			while True:
+				rst = raw_input("Send RST message? [y/N]: ")
+				if rst != "" and not (rst.lower() == "n" or rst.lower() == "y"):
+					print "Unrecognized choose."
+					continue
+				elif rst == "" or rst.lower() == "y":
+					serverRequest.cancel_observing(response, True)
+				else:
+					serverRequest.cancel_observing(response, False)
+				check = False
+				break
 		else:
-			print "[**ERROR**] No response has been received from the server.\n"
-		time.sleep(5)
+			continue
+# End of client_callback_observe()
+
+
+def simple_request():
+	host, port, path = create_pkt()
+	print "[{0}] Making GET request: [{1}]:{2}{3}".format(time.ctime(), host,port,path)
+	serverRequest = HelperClient(server=(host, port))
+	if serverRequest:
+		response = serverRequest.get(path)
+		if response:
+			serverRequest.stop()
+			write_file(response)
+	else:
+		print "[**ERROR**] No response has been received from the server.\n"
+# End of simple_request()
+
+
+def main():
+	print "----------------------------------------------------------"
+	type_of_request = raw_input("Choose to do a simple Request(R) or to set an Observer(O): ")
+	if type_of_request == "" or not (type_of_request.upper()=="R" or type_of_request.upper()=="O"):
+		print "Unrecognized choose."
+	elif type_of_request.upper()=="R":
+		timer_req = raw_input("   Choose a timer to make periodical requests (0 for only one): ")
+		if timer_req=="0":
+			simple_request()
+		elif timer_req>"0":
+			while (1):
+				simple_request()
+				time.sleep(float(timer_req))
+		else:
+			print "Unrecognized choose."
+
+	elif type_of_request.upper()=="O":
+		observer_func()
+
+
 # End of main()
 
 if __name__ == '__main__':
